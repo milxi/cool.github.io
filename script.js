57212306768912ff2acf46b4a9d54078b72f1b28
milxi <32701622+milxi@users.noreply.github.com>
2017-10-13 11:30:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+console.log("Hello");
+
+/* Basic math calculations*/
+console.log(5*5);
+console.log(10/5);
+
+/*Gives a random number between 0-1 and the * times it by 100. */
+console.log(Math.random()*100);
+
+/* THIS IS USED TO SEE IF A FIVE IS BIGGER THAN 4, THIS WILL GIVE THE ANSWER.*/
+if(5>4){
+	console.log("it is true");
+}
+
+else{
+	console.log("it is false!");
+}
+
+/* Now we gdoing a function which you can name to anything. eka kirjain aina pieni, siitä eteenpäin isolla eka jos pidempi nimi. */
+function printApples(unknowm){
+	while (unknown<15){
+		console.log("I have an apple");
+		unknown+=1;
+
+	}
+}
+
+/* This is the call for the function above*/
+printApples(10);
+
+/* ADDS THEXT TO THE EMPTY SLOT ON MY HTML*/
+document.getElementById('pop').innerHTML="put some text here"
+
+
+
+function change(){
+	
+}
+
